68b67e17224b5cae502352666c51d041fe0c3385
Shaban <shaban@teachaway.com>
2019-06-07 17:30:16 +0200

    minor fix

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -30,7 +30,7 @@ const Login = () => {
 
 const Logout = () => {
   return (
-    <span className="nav-link port-navbar-link clickable"> Logout </span>
+    <span onClick={auth0.logout} className="nav-link port-navbar-link clickable"> Logout </span>
   )
 };
 
@@ -71,12 +71,16 @@ export default class Header extends React.Component {
               <NavItem  className="port-navbar-item">
                 <BsNavLink route="/cv" title="Cv"/>
               </NavItem>
+              { !auth0.isAuthenticated() &&
               <NavItem  className="port-navbar-item">
                 <Login />
               </NavItem>
-              <NavItem  className="port-navbar-item">
-                <Logout />
-              </NavItem>
+              }
+              { auth0.isAuthenticated() &&
+                <NavItem className="port-navbar-item">
+                  <Logout />
+                </NavItem>
+              }
             </Nav>
           </Collapse>
         </Navbar>
--- services/auth0.js
+++ services/auth0.js
@@ -1,4 +1,5 @@
 import auth0 from 'auth0-js';
+import Cookies from 'js-cookie';
 
 class Auth0 {
 
@@ -12,11 +13,12 @@ class Auth0 {
     });
 
     this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
     this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
   }
 
   handleAuthentication() {
-    debugger;
     return new Promise((resolve, reject) => {
       this.auth0.parseHash((err, authResult) => {
         if (authResult && authResult.accessToken && authResult.idToken) {
@@ -30,12 +32,52 @@ class Auth0 {
     });
   }
 
-  setSession() {
-    // Save tokens!!!!
+  setSession(authResult) {
+    // Set the time that the Access Token will expire at
+    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    //this.accessToken = authResult.accessToken;
+
+    Cookies.set('user', authResult.idTokenPayload);
+    Cookies.set('jwt', authResult.idToken);
+    Cookies.set('expiresAt', expiresAt);
+
+  }
+
+  logout() {
+    Cookies.remove('user');
+    Cookies.remove('jwt');
+    Cookies.remove('expiresAt');
+
+    this.auth0.logout({
+      returnTo: '',
+      clientID: 'i4equOOn6pPgHy8XoRSWz5ZRNQhbLqLD'
+    });
   }
 
   login() {
     this.auth0.authorize();
+  }
+
+  isAuthenticated() {
+    const expiresAt = Cookies.getJSON('expiresAt');
+    return new Date().getTime() < expiresAt;
+  }
+
+  clientAuth() {
+    return this.isAuthenticated();
+  }
+
+  serverAuth(req) {
+    if (req.headers.cookie) {
+
+      const expiresAtCookie = req.headers.cookie.split(';').find(c => c.trim().startsWith('expiresAt='));
+
+      if(!expiresAtCookie) { return undefined };
+
+      const expiresAt = expiresAtCookie.split('=')[1];
+
+      return new Date().getTime() < expiresAt;
+    }
   }
 }
 
